0238e405d77282de95ae64ae39110ef8e7bb212a
Shinas <shinaskvzr@gmail.com>
2025-08-16 18:10:12 +0530

    email js

---
+++ index.js
@@ -0,0 +1,26 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+    
+    const serviceID = "service_qjmq8h7";
+    const templateID = "template_g6gowht";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.getElementById("name").value = "";
+                document.getElementById("phone").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+                console.log(res);
+                alert("Your message sent successfully")
+            }
+        )
+        .catch(err => console.log(err));
+
+}
+
